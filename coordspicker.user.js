41ecdb11c0c32261aff1b62bf66b3840ff3d14ee
PTAsuma <67612941+PTAsuma@users.noreply.github.com>
2020-07-17 00:53:16 +0100

    Update coordspicker.user.js

--- coordspicker.user.js
+++ coordspicker.user.js
@@ -1,1 +1,15 @@
-asdas
+document.body.onkeydown = function() {
+    if (event.keyCode == 67) {
+        var coords2 = document.getElementById('coords').innerHTML
+        var coords = coords2.replace(/ /g,'')
+        var chati = document.getElementById('chat').value
+        var x = coords.substring(coords.indexOf(":") + 1, coords.indexOf("Y"))
+        var y = coords.substring(coords.lastIndexOf(":") + 1, coords.length)
+        if (chati.includes('[' && ']')) {
+            document.getElementById('chat').value = document.getElementById('chat').value.replace(/\[\d+ \d+\]/, `[${x} ${y}]`);
+
+        } else {
+            document.getElementById('chat').value += ` [${x} ${y}]`
+        }
+    }
+}
